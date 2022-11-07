0321884744fd5267bca01d3ae3fcac1c74c34e6a
hennyerlianti <117568490+hennyerlianti@users.noreply.github.com>
2022-11-07 09:12:35 +0700

    Latihan 1

---
+++ latihanPBO/src/PackageLatihanPBO/Biodata.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PackageLatihanPBO;
+
+/**^
+ *
+ * @author acer
+ */
+public class Biodata {
+    public static void main(String[] args) {
+        //ini adalah Henny
+        System.out.println("Nama    : Henny Erlianti");
+        System.out.println("Nim     : 220140021");
+        System.out.println("Alamat  : Pajukungan");
+    }
+}
