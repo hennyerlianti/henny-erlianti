8530bcb329fed67fb008b2f2a3263b3b3058fede
hennyerlianti <117568490+hennyerlianti@users.noreply.github.com>
2022-11-07 09:20:29 +0700

    java pertama(latihan 1)

---
+++ PBO_Latihan1/src/pbo_latihan1/latihan.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_latihan1;
+
+/**
+ *
+ * @author acer
+ */
+public class latihan {
+   public static voic main(String[]args){
+       System.out.println("Hallo, ini java pertama saya");
+   }
+}
